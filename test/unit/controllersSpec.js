ae81f4c1093f89030111cf6e9f7ec732248dd768
William Montaz <wmontaz.prestataire@voyages-sncf.com>
2014-05-05 17:18:29 +0200

    Update du modele instance

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -34,35 +34,281 @@ describe('controllers', function(){
 		expect(scope.instances).toEqualData(
 			[
 				{
-					"application": "WDI",
-					"platform": "INT1",
-					"client": "SAB",
-                    "type": "WAS",
-					"composant": "WDI",
-					"name": "WDIGALI11WDI",
-					"hostname": "GALERIA",
-					"ip": "10.16.34.92"
-				},
-				{
-					"application": "WDI",
-					"platform": "INT1",
-					"client": "SAB",
-                    "type": "WAS",
-					"composant": "WDI",
-					"name": "WDIGALI12WDI",
-					"hostname": "GALERIA",
-					"ip": "10.16.34.92"
-				},
-				{
-					"application": "WDI",
-					"platform": "INT1",
-					"client": "SAB",
-                    "type": "WAS",
-					"composant": "WDI",
-					"name": "WDIGALI13WDI",
-					"hostname": "GALERIA",
-					"ip": "10.16.34.92"
-				}
+							"id": 1,
+							"type": "WAS",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"composant": "WDI",
+							"user": "vmomiwu5",
+							"home": "/appl/vmomiwu5/GST",
+							"puppet_template_path": "miw/miw/v1",
+							"name": "VMODENU51GSTMIW",
+							"hostname": "DENOEL",
+							"ip": "10.98.208.67",
+							"bins": [
+								{
+									"name": "TOMCAT",
+									"version": "7.0.37",
+									"home": "/export/product/tomcat/product/7.0.37"
+								},
+								{
+									"name": "JDK",
+									"version": "1.7u15",
+									"home": "/usr/java/vsc_jdk_1.7u15_32bit",
+								}
+							],
+							"modules": [
+								{
+									"name": "miui",
+									"war": "mi-ui.war",
+									"context": "miwUi"
+								},
+								{
+									"name": "miIolog",
+									"war": "mi-admin-iolog.war",
+									"context": "miwIolog"
+								}
+							],
+							"external_links": {
+								"jdbc": [
+									{
+										"name": "wdi/ds",
+										"schema": "uiwivmo1",
+										"pool": {
+											"min": 1,
+											"max": 5
+										}
+									}
+								],
+								"jms": [
+									{
+										"name": "ConnectionFactoryMetrix",
+										"type": "AMQ",
+										"shortName": "MTX",
+										"instance": "VMOCLOU51AMQMTX",
+									}
+								],
+								"jolt": [
+									{
+										"hosts": "TUX1",
+										"gateway": "joltpoolSN",
+										"pool": {
+											"min": 1,
+											"max": 5
+										},
+										"instance_id": "6"
+									}
+								],
+								"was": [
+									{
+										"name": "ESB",
+										"instances": ["VMODENU51MIW"]
+									},
+								]
+							},
+							"jvm_opts": {
+								"tuning": ["-Xmsx=1024"],
+								"system": ["-Dsyslog=localhost:3128"]
+							},
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 55010
+								},
+								{
+									"name": "JMX",
+									"number": 55011
+								}
+							]
+						},
+						{
+							"id": 2,
+							"type": "WAS",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"composant": "WDI",
+							"user": "vmomiwu5",
+							"home": "/appl/vmomiwu5/GST",
+							"puppet_template_path": "miw/miw/v1",
+							"name": "VMODENU52GSTMIW",
+							"hostname": "DENOEL",
+							"ip": "10.98.208.67",
+							"bins": [
+								{
+									"name": "TOMCAT",
+									"version": "7.0.37",
+									"home": "/export/product/tomcat/product/7.0.37"
+								},
+								{
+									"name": "JDK",
+									"version": "1.7u15",
+									"home": "/usr/java/vsc_jdk_1.7u15_32bit",
+								}
+							],
+							"modules": [
+								{
+									"name": "miui",
+									"war": "mi-ui.war",
+									"context": "miwUi"
+								},
+								{
+									"name": "miIolog",
+									"war": "mi-admin-iolog.war",
+									"context": "miwIolog"
+								}
+							],
+							"external_links": {
+								"jdbc": [
+									{
+										"name": "wdi/ds",
+										"schema": "uiwivmo1",
+										"pool": {
+											"min": 1,
+											"max": 5
+										}
+									}
+								],
+								"jms": [
+									{
+										"name": "ConnectionFactoryMetrix",
+										"type": "AMQ",
+										"shortName": "MTX",
+										"instance": "VMOCLOU51AMQMTX",
+									}
+								],
+								"jolt": [
+									{
+										"hosts": "TUX1",
+										"gateway": "joltpoolSN",
+										"pool": {
+											"min": 1,
+											"max": 5
+										},
+										"instance_id": "6"
+									}
+								],
+								"was": [
+									{
+										"name": "ESB",
+										"instances": ["VMODENU51MIW"]
+									},
+								]
+							},
+							"jvm_opts": {
+								"tuning": ["-Xmsx=1024"],
+								"system": ["-Dsyslog=localhost:3128"]
+							},
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 55010
+								},
+								{
+									"name": "JMX",
+									"number": 55011
+								}
+							]
+						},
+						{
+							"id": 3,
+							"type": "WAS",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"composant": "WDI",
+							"user": "vmomiwu5",
+							"home": "/appl/vmomiwu5/GST",
+							"puppet_template_path": "miw/miw/v1",
+							"name": "VMODENU53GSTMIW",
+							"hostname": "DENOEL",
+							"ip": "10.98.208.67",
+							"bins": [
+								{
+									"name": "TOMCAT",
+									"version": "7.0.37",
+									"home": "/export/product/tomcat/product/7.0.37"
+								},
+								{
+									"name": "JDK",
+									"version": "1.7u15",
+									"home": "/usr/java/vsc_jdk_1.7u15_32bit",
+								}
+							],
+							"modules": [
+								{
+									"name": "miui",
+									"war": "mi-ui.war",
+									"context": "miwUi"
+								},
+								{
+									"name": "miIolog",
+									"war": "mi-admin-iolog.war",
+									"context": "miwIolog"
+								}
+							],
+							"external_links": {
+								"jdbc": [
+									{
+										"name": "wdi/ds",
+										"schema": "uiwivmo1",
+										"pool": {
+											"min": 1,
+											"max": 5
+										}
+									}
+								],
+								"jms": [
+									{
+										"name": "ConnectionFactoryMetrix",
+										"type": "AMQ",
+										"shortName": "MTX",
+										"instance": "VMOCLOU51AMQMTX",
+									}
+								],
+								"jolt": [
+									{
+										"hosts": "TUX1",
+										"gateway": "joltpoolSN",
+										"pool": {
+											"min": 1,
+											"max": 5
+										},
+										"instance_id": "6"
+									}
+								],
+								"was": [
+									{
+										"name": "ESB",
+										"instances": ["VMODENU51MIW"]
+									},
+								]
+							},
+							"jvm_opts": {
+								"tuning": ["-Xmsx=1024"],
+								"system": ["-Dsyslog=localhost:3128"]
+							},
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 55010
+								},
+								{
+									"name": "JMX",
+									"number": 55011
+								}
+							]
+						}
 			]
 		);
 		

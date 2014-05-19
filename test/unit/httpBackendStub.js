325df9a9c559867aecf0ee74eec7430d5d81852d
William Montaz <wmontaz.prestataire@voyages-sncf.com>
2014-05-19 18:57:32 +0200

    Ajout des types ESB AMQ TUX ATO HTTP HAP dans le stub http

--- test/unit/httpBackendStub.js
+++ test/unit/httpBackendStub.js
@@ -49,7 +49,6 @@ Test.wdiInstances = [
 									},
 									"configuration": {
 										"name": "wdi/ds",
-										"schema": "uiwivmo1",
 										"pool_min": 1,
 										"pool_max": 5
 									}
@@ -58,11 +57,11 @@ Test.wdiInstances = [
 									"type": "JMS",
 									"key": {
 										"name": "ConnectionFactoryMetrix",
-										"shortName": "MTX",
 										"instance": "VMOCLOU51AMQMTX"
 									},
 									"configuration": {
-										"type": "AMQ"
+										"type": "AMQ",
+										"amq_type": "MTX",
 									}
 								},
 								{
@@ -148,7 +147,6 @@ Test.wdiInstances = [
 									},
 									"configuration": {
 										"name": "wdi/ds",
-										"schema": "uiwivmo1",
 										"pool_min": 1,
 										"pool_max": 5
 									}
@@ -247,7 +245,6 @@ Test.wdiInstances = [
 									},
 									"configuration": {
 										"name": "wdi/ds",
-										"schema": "uiwivmo1",
 										"pool_min": 1,
 										"pool_max": 5
 									}
@@ -330,6 +327,414 @@ Test.wdiInstances = [
 							"password": "uiwivmo2",
 							"hostname": "GRIANTE",
 							"ip": "10.98.208.68",
+						},
+						{
+							"id": 6,
+							"type": "ESB",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "MIW",
+							"name": "VMODENU51MIW",
+							"hostname": "DENOEL",
+							"ip": "10.98.208.67",
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 55030
+								},
+								{
+									"name": "JMX",
+									"number": 55031
+								}
+							],
+							"links": [
+								{
+									"type": "WAS",
+									"key": {
+										"name": "WDI",
+										"schema": "VMODENU51WDI"
+									}
+								}
+							],
+							"modules": [
+								{
+									"name": "ROOT",
+									"war": "mulemiw-esbexplode",
+									"context": "miwMule"
+								}
+							]
+						},
+						{
+							"id": 7,
+							"type": "TUX",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "WDI",
+							"name": "joltpoolSN",
+							"hostname": "",
+							"ip": "148.169.132.44",
+							"ports": [
+								{
+									"name": "TUXEDO",
+									"number": 51813
+								}
+							]
+						},
+						{
+							"id": 8,
+							"type": "TUX",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "CBE",
+							"name": "joltpoolSN",
+							"hostname": "",
+							"ip": "148.169.132.44",
+							"ports": [
+								{
+									"name": "TUXEDO",
+									"number": 51813
+								}
+							]
+						},
+						{
+							"id": 9,
+							"type": "ATO",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "ATOS",
+							"name": "joltpoolSN",
+							"user": "atosvmo",
+							"home": "/appl/apiatosvmo",
+							"hostname": "DENOEL",
+							"ip": "148.169.132.44",
+							"bins": [
+								{
+									"name": "JDK",
+									"version": "1_5_0_17",
+									"home": "/usr/java/vsc_jdk_1.5u17_32bit"
+								},
+								{
+									"name": "ATOS",
+									"version": "320-1.320-2",
+									"home": "/HOME/atos/product/API_Atos_o320_s204_c107",
+								}
+							],
+							"ports": [
+								{
+									"name": "SERVICE",
+									"number": 9185
+								},
+								{
+									"name": "COMMAND",
+									"number": 9186
+								},
+								{
+									"name": "HTTP",
+									"number": 9187
+								},
+								{
+									"name": "SURV",
+									"number": 9188
+								}
+							]
+						},
+						{
+							"id": 9,
+							"type": "AMQ",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "AMQ",
+							"name": "VMOCLOU51AMQMTA",
+							"user": "amqvslu5",
+							"home": "/appl/amqvslu5",
+							"hostname": "",
+							"amq_type" : "MTA",
+							"logical_name": "mail",
+							"queue": "mail-broker-vmousn5",
+							"target_dir": "/appl/amqvslu5/mail/data/",
+							"discovery_url": "'230.0.0.251:52830?group=mail-vmousn5'",
+							"ip": "10.98.208.66",
+							"bins": [
+								{
+									"name": "JDK",
+									"version": "1.6u37",
+									"home": "/usr/java/vsc_jdk_1.6u37_32bit"
+								},
+								{
+									"name": "AMQ",
+									"version": "5.4.1-fuse-02-00",
+									"home": "/HOME/activemq/product/5.4.1-fuse-02-00",
+								}
+							],
+							"ports": [
+								{
+									"name": "BROKER",
+									"number": 55051
+								},
+								{
+									"name": "JETTY",
+									"number": 51052
+								},
+								{
+									"name": "TRANSPORT",
+									"number": 55053
+								},
+								{
+									"name": "URI",
+									"number": 55054
+								}
+							]
+						},
+						{
+							"id": 10,
+							"type": "AMQ",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "AMQ",
+							"name": "VMOCLOU51AMQMTA",
+							"user": "amqvslu5",
+							"home": "/appl/amqvslu5",
+							"hostname": "",
+							"amq_type" : "MTX",
+							"logical_name": "mail",
+							"queue": "metrix-broker-vmousn5",
+							"target_dir": "/appl/amqvslu5/metrix/data/",
+							"discovery_url": "'230.0.0.251:52830?group=metrix-vmousn5'",
+							"ip": "10.98.208.66",
+							"bins": [
+								{
+									"name": "JDK",
+									"version": "1.6u37",
+									"home": "/usr/java/vsc_jdk_1.6u37_32bit"
+								},
+								{
+									"name": "AMQ",
+									"version": "5.4.1-fuse-02-00",
+									"home": "/HOME/activemq/product/5.4.1-fuse-02-00",
+								}
+							],
+							"ports": [
+								{
+									"name": "BROKER",
+									"number": 55061
+								},
+								{
+									"name": "JETTY",
+									"number": 51062
+								},
+								{
+									"name": "TRANSPORT",
+									"number": 55063
+								},
+								{
+									"name": "URI",
+									"number": 55064
+								}
+							]
+						},
+						{
+							"id": 11,
+							"type": "HTTP",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "VMO",
+							"name": "VMONOVI51",
+							"user": "webadm",
+							"home": "/HOME/webadm/VMO",
+							"target_dir": "webvmo",
+							"method_test": 1,
+							"nb_echec": 10,
+							"hostname": "NOVELLA",
+							"ip": "10.102.99.100",
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 80
+								},
+								{
+									"name": "HTTPS",
+									"number": 443
+								}
+							],
+							"links": [
+								{
+									"type": "HAP",
+									"key": {
+										"name": "/VMO",
+										"instance": "VMOINT5VMO"
+									}
+								}
+							]
+						},
+						{
+							"id": 12,
+							"type": "HAP",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "VMO",
+							"name": "VMOINT5VMO",
+							"user": "hapadm",
+							"home": "/HOME/hapadm/VMO",
+							"target_dir": "vmo",
+							"hostname": "MONREALE",
+							"ip": "10.101.175.71",
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 51100
+								}
+							],
+							"links": [
+								{
+									"type": "WAS",
+									"key": {
+										"name": "/mobile",
+										"instance": "VMOEVII51VMO"
+									}
+								},
+								{
+									"type": "WAS",
+									"key": {
+										"name": "/mobile",
+										"instance": "VMOEVII52VMO"
+									}
+								}
+							]
+						},
+						{
+							"id": 13,
+							"type": "HAP",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "VMO",
+							"name": "VMOINT5VMOCBE",
+							"user": "hapadm",
+							"home": "/HOME/hapadm/VMO",
+							"target_dir": "vmo",
+							"hostname": "MONREALE",
+							"ip": "10.101.175.71",
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 51101
+								}
+							],
+							"links": [
+								{
+									"type": "WAS",
+									"key": {
+										"name": "/cbeWebServices",
+										"instance": "VMOGALI51CBE"
+									}
+								}
+							]
+						},
+						{
+							"id": 14,
+							"type": "HAP",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "VMO",
+							"name": "VMOINT5VMO",
+							"user": "hapadm",
+							"home": "/HOME/hapadm/VMO",
+							"target_dir": "vmo",
+							"hostname": "MONTE",
+							"ip": "10.101.175.71",
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 51100
+								}
+							],
+							"links": [
+								{
+									"type": "WAS",
+									"key": {
+										"name": "/mobile",
+										"instance": "VMOEVII51VMO"
+									}
+								},
+								{
+									"type": "WAS",
+									"key": {
+										"name": "/mobile",
+										"instance": "VMOEVII52VMO"
+									}
+								}
+							]
+						},
+						{
+							"id": 15,
+							"type": "HAP",
+							"application": "WDI",
+							"application_fullname": "Application WDI",
+							"application_version": "2013-04",
+							"application_url": "usl.wsmobile.voyages-sncf.com",
+							"platform": "INT1",
+							"client": "SAB",
+							"component": "VMO",
+							"name": "VMOINT5VMO",
+							"user": "hapadm",
+							"home": "/HOME/hapadm/VMO",
+							"target_dir": "vmo",
+							"hostname": "MONTE",
+							"ip": "10.101.175.71",
+							"ports": [
+								{
+									"name": "HTTP",
+									"number": 51101
+								}
+							],
+							"links": [
+								{
+									"type": "WAS",
+									"key": {
+										"name": "/cbeWebServices",
+										"instance": "VMOGALI51CBE"
+									}
+								}
+							]
 						}
 					];
 Test.singleInstance = {
@@ -446,7 +851,7 @@ Test.prepareHttpBackendStub = function($httpBackend) {
 				$httpBackend.whenGET('/rest/instances/1').
 					respond(function(){ return [200, Test.singleInstance, {}]});
 					
-				$httpBackend.whenGET('/search?application=WDI&platform=INT1').
+				$httpBackend.whenGET('/rest/search?application=WDI&platform=INT1').
 					respond(function(){ return [200, Test.wdiInstances, {}]});
 								
 				$httpBackend.whenGET('/rest/instances').

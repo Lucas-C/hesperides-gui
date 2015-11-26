e5ed5dafd0f49c8f787e199ad378117745d8e36e
emeric_martineau <emartineau@voyages-sncf.com>
2015-11-26 09:07:49 +0100

    Ajout de la substitution automatique de la version dans le fichier menu.js

--- app/menu/menu.js
+++ app/menu/menu.js
@@ -145,7 +145,7 @@ menuModule.controller('MenuHelpCtrl', ['$scope', '$modal', '$http', function($sc
 
     $scope.display_hesperides_informations = function(){
 
-        $scope.front_version = '0.2.6-SNAPSHOT';
+        $scope.front_version = '${project.version}';
 
         //Get the backend versions
         $http.get('rest/versions').then(function(response){
